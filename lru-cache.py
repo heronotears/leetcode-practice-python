8f9cfe4aea4d383cd7aa64656276a2a4ec7a6e56
Ryan Fau <lazyboy.fan@gmail.com>
2020-05-25 19:50:32 +0800

    146. LRU缓存机制(https://leetcode-cn.com/problems/lru-cache/)

---
+++ lru-cache.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+"""146. LRU缓存机制(https://leetcode-cn.com/problems/lru-cache/)
+"""
+
+import collections
+import unittest
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = collections.OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key, last=True)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.cap:
+            self.cache.popitem(last=False)
+
+
+class TestCase(unittest.TestCase):
+
+    def test_lru(self):
+        cache = LRUCache(2)
+        cache.put(1, 1)
+        cache.put(2, 2)
+        self.assertEqual(cache.get(1), 1)
+        cache.put(3, 3)
+        self.assertEqual(cache.get(2), -1)
+        cache.put(4, 4)
+        self.assertEqual(cache.get(1), -1)
+        self.assertEqual(cache.get(3), 3)
+        self.assertEqual(cache.get(4), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
